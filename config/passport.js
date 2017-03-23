6dd751093b760976129d1eb305bba8a227ed8f8f
Daan <daanraatjes@gmail.com>
2017-03-23 22:48:16 +0100

    separation of files

--- config/passport.js
+++ config/passport.js
@@ -14,15 +14,15 @@ var users = [{
 module.exports = function(passport) {
      
     
-passport.use('login', new LocalStrategy(
-    function (username, password, done) {
-        if (username === users[0].username && password === users[0].password) {
-            return done(null, users[0]);
-        } else {
-            return done(null, false, {"message": "Invalid username or password."});
-        }
-    })
-);
+	passport.use('login', new LocalStrategy(
+	    function (username, password, done) {
+	        if (username === users[0].username && password === users[0].password) {
+	            return done(null, users[0]);
+	        } else {
+	            return done(null, false, {"message": "Invalid username or password."});
+	        }
+	    })
+	);
 
     passport.serializeUser(function(user, done) {
       done(null, users[0]._id);
@@ -33,6 +33,6 @@ passport.use('login', new LocalStrategy(
         done(null, users[0]);
     });
 
-  
+   
 
 };
