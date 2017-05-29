d90a47c3a13ddc5c71efbaa28d4bf5ea594be89d
Daan <daanraatjes@gmail.com>
2017-05-29 16:47:11 +0200

    Remove login for testing purposes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('./config/initialiseApp')(app);
 var control = require('./config/addControllers')(app);
 control.addControllers();
 
+
 var defaultPort = process.env.PORT || 8080;
 app.listen(defaultPort, function () {
     console.log('Our app is running on port:' + defaultPort);
