f1f327f5cf751f94057a060d49a75ec765833f48
Daan <daanraatjes@gmail.com>
2017-06-06 15:16:32 +0200

    Fixed ip and port for server

--- config/dayManipulation.js
+++ config/dayManipulation.js
@@ -4,13 +4,11 @@ var json = require('../config/getJSON.js');
 
 
 exports.getWeekEvents = function (path, callback) {
-    var hostname = path.split(':')[0];
-    var port     = path.split(':')[1];
     json.getJSON({
         type: 'GET',
         path: '/calendar/event?week=' + 0 + '&extended=true',
-        host: hostname,
-        port: port,
+        host: 'localhost',
+        port: '8800',
         async: false,
         dataType: 'json'
     }, function (statusCode, data) {
