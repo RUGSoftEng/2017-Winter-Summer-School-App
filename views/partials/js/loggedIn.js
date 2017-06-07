b97272157ab7cfb6afd0625c56c613037f060372
Daan <daanraatjes@gmail.com>
2017-06-07 14:53:21 +0200

    Fix modal buttons

--- views/partials/js/loggedIn.js
+++ views/partials/js/loggedIn.js
@@ -1,126 +1,122 @@
- /*
- 	This script handles all UI manipulations for the modal. It dynamically changes
- 	the input fields, buttons and texts of the modal depending on where is clicked.
-  */
+/*
+ This script handles all UI manipulations for the modal. It dynamically changes
+ the input fields, buttons and texts of the modal depending on where is clicked.
+ */
 
- var titles           = ["Add an announcement", "Add general information", "Add a new event"];
- var editTitles       = ["Edit the announcement", "Edit general information", "Edit event"];
- var buttonTexts      = ["Post announcement", "Add section", "Submit event"];
- var sectionHeaders   = ["Title of announcement", "Information header", "Event summary"];
- var links            = ["/announcement/item", "/generalinfo/item", "calendar/event"];
- var days             = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+var titles         = ["Add an announcement", "Add general information", "Add a new event"];
+var editTitles     = ["Edit the announcement", "Edit general information", "Edit event"];
+var buttonTexts    = ["Post announcement", "Add section", "Submit event"];
+var sectionHeaders = ["Title of announcement", "Information header", "Event summary"];
+var links          = ["/announcement/item", "/generalinfo/item", "calendar/event"];
+var days           = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
- var modalSelector         = '#add-announcement ';
- var titleSelector         = '#announcementTitle ';
- var descriptionSelector   = '#announcementDescription ';
- var eventDetailsSelector  = '#details ';
- var eventLocationSelector = '#location ';
+var modalSelector         = '#add-announcement ';
+var titleSelector         = '#announcementTitle ';
+var descriptionSelector   = '#announcementDescription ';
+var eventDetailsSelector  = '#details ';
+var eventLocationSelector = '#location ';
 
- function addNewItem(type, edit) {
-     toggleButtons('bDeFP');
+function addNewItem(type, edit) {
+    toggleButtons('bdeFP');
 
-     if (edit) {
-         getButton('f').data('type', 'edit');
-     } else {
-         getButton('f').data('type', 'add');
-         toggleButtons('d'); // hide delete if we're not editing
-     }
-     $(modalSelector + '.modal-title').html("<p>" + (edit ? editTitles[$type] : titles[$type]) + "</p>");
-     $('.description-form').show();
-     $(modalSelector + '#section-header').html(sectionHeaders[$type]);
-     getButton('f').html((edit ? "Edit" : buttonTexts[$type]));
-     $(modalSelector + 'form').attr('action', links[$type]);
-     toggleShow(false);
-     $('.datetime-form').hide();
-     $(modalSelector + '.target').show();
-     if($type == 2) { // adding a schedule event
-     	 toggleButtons('p'); // no preview button for scheduling
-	     $('.description-form').hide();
-	     $('.datetime-form').show();
-     }
-     toggleScheduleInput(false);
+    getButton('f').data('type', edit ? 'edit' : 'add');
 
- };
+    $(modalSelector + '.modal-title').html("<p>" + (edit ? editTitles[$type] : titles[$type]) + "</p>");
+    $('.description-form').show();
+    $(modalSelector + '#section-header').html(sectionHeaders[$type]);
+    getButton('f').html((edit ? "Save" : buttonTexts[$type]));
+    $(modalSelector + 'form').attr('action', links[$type]);
+    toggleShow(false);
+    $('.datetime-form').hide();
+    $(modalSelector + '.target').show();
+    if ($type == 2) { // adding a schedule event
+        toggleButtons('p'); // no preview button for scheduling
+        $('.description-form').hide();
+        $('.datetime-form').show();
+    }
+    toggleScheduleInput(false);
 
- function displayItem(title, text, $type) {
-     $(modalSelector + '.target').hide();
-     $(modalSelector + '.modal-title').html(title);
-     $(modalSelector + '.modal-show-body .jumbotron').html(text);
-     $(modalSelector).data("type", $type);
-     toggleShow(true);
-     toggleButtons('bEfpd');
- };
+};
 
- function openTodaysSchedule() {
-	 $today = days[new Date().getDay()]; // take the positive modulo
- 	 $('#' + $today).addClass('in');
- }
+function displayItem(title, text, $type) {
+    $(modalSelector + '.target').hide();
+    $(modalSelector + '.modal-title').html(title);
+    $(modalSelector + '.modal-show-body .jumbotron').html(text);
+    $(modalSelector).data("type", $type);
+    toggleShow(true);
+    toggleButtons('bEfpD');
+};
 
- function initialiseScheduleDatePicker() {
-	 $( "#scheduleStartDate, #scheduleEndDate" ).datepicker({
-	     dateFormat: 'yy-mm-dd'
-     });
- }
+function openTodaysSchedule() {
+    $today = days[new Date().getDay()]; // take the positive modulo
+    $('#' + $today).addClass('in');
+}
 
- function emptyContainer(selector) {
-	 $(selector).val('');
- }
+function initialiseScheduleDatePicker() {
+    $("#scheduleStartDate, #scheduleEndDate").datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+}
 
- function toggleScheduleInput(disabled) {
- 	 var sc = '.modal-add-body .form-group ';
-	 $(sc + 'input, ' + sc + 'textarea, ' + sc + 'select').attr("disabled", disabled);
- }
+function emptyContainer(selector) {
+    $(selector).val('');
+}
 
- function fillScheduleInput(clicked) {
-	 $(titleSelector).val(clicked.data('event-summary'));
-	 $(eventLocationSelector).val(clicked.data('event-location'));
-	 $(eventDetailsSelector).val(clicked.data('event-description'));
-	 $('#scheduleStartDate').val(clicked.data('event-start-date').substring(0,10));
-	 $('#scheduleEndDate').val(clicked.data('event-end-date').substring(0,10));
-	 $('#startHour').val(clicked.data('event-start-date').substring(11,13));
-	 $('#startMinute').val(clicked.data('event-start-date').substring(14,16));
-	 $('#endHour').val(clicked.data('event-end-date').substring(11,13));
-	 $('#endMinute').val(clicked.data('event-end-date').substring(14,16));
-     $('#targetItem').val(clicked.data('event-ssid'));
- }
+function toggleScheduleInput(disabled) {
+    var sc = '.modal-add-body .form-group ';
+    $(sc + 'input, ' + sc + 'textarea, ' + sc + 'select').attr("disabled", disabled);
+}
 
- function initialiseModalOpeners() {
-	 $('.open-modal').click(function() {
-         $(modalSelector).data('id', $(this).data('id'));
-         toggleButtons('d');
-         $type = $(this).data("type");
+function fillScheduleInput(clicked) {
+    $(titleSelector).val(clicked.data('event-summary'));
+    $(eventLocationSelector).val(clicked.data('event-location'));
+    $(eventDetailsSelector).val(clicked.data('event-description'));
+    $('#scheduleStartDate').val(clicked.data('event-start-date').substring(0, 10));
+    $('#scheduleEndDate').val(clicked.data('event-end-date').substring(0, 10));
+    $('#startHour').val(clicked.data('event-start-date').substring(11, 13));
+    $('#startMinute').val(clicked.data('event-start-date').substring(14, 16));
+    $('#endHour').val(clicked.data('event-end-date').substring(11, 13));
+    $('#endMinute').val(clicked.data('event-end-date').substring(14, 16));
+    $('#targetItem').val(clicked.data('event-ssid'));
+}
 
-         if ($(this).data("show") != "overview") {
-         	 emptyContainer(eventDetailsSelector);
-			 emptyContainer(eventLocationSelector);
-         	 if($(this).data("spec") != "schedule") {
-             	addNewItem($type, false);
-             	emptyContainer(titleSelector);
-			 	emptyContainer(descriptionSelector);
-			 	$(modalSelector).data('show', 'new');
-             }
-             else {
-             	addNewItem($type, true);
-             	$(modalSelector).data("type", $type);
-			 	$(modalSelector).data('id', $(this).data('event-id'));
-             	toggleButtons('bEfpd');
-             	toggleScheduleInput(true);
-             	fillScheduleInput($(this));
-             }
+function initialiseModalOpeners() {
+    $('.open-modal').click(function () {
+        $(modalSelector).data('id', $(this).data('id'));
+        toggleButtons('d');
+        $type = $(this).data("type");
+
+        if ($(this).data("show") != "overview") {
+            emptyContainer(eventDetailsSelector);
+            emptyContainer(eventLocationSelector);
+            if ($(this).data("spec") != "schedule") {
+                addNewItem($type, false);
+                emptyContainer(titleSelector);
+                emptyContainer(descriptionSelector);
+                $(modalSelector).data('show', 'new');
+            }
+            else {
+                addNewItem($type, true);
+                $(modalSelector).data("type", $type);
+                $(modalSelector).data('id', $(this).data('event-id'));
+                toggleButtons('bEfpd');
+                toggleScheduleInput(true);
+                fillScheduleInput($(this));
+            }
 
 
-         } else {
-             $(modalSelector).data('show', 'known');
-             displayItem($(this).find('span.title').html(), $(this).find('.data-text').html(), $type);
+        } else {
+            $(modalSelector).data('show', 'known');
+            displayItem($(this).find('span.title').html(), $(this).find('.data-text').html(), $type);
 
-         }
-     });
- }
+        }
+    });
+}
 
- $(function() {
-	 openTodaysSchedule();
-	 initialiseScheduleDatePicker();
-   initialiseScheduleButtons();
-	 initialiseButtons();
-	 initialiseModalOpeners();
- });
+$(function () {
+    openTodaysSchedule();
+    initialiseScheduleDatePicker();
+    initialiseScheduleButtons();
+    initialiseButtons();
+    initialiseModalOpeners();
+});
