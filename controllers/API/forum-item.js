ca8e7e887669fdcad3b442f5fc36bfcb7758df5b
Nikolas Hadjipanayi <nikolas.hadjipanayi@gmail.com>
2017-06-08 03:10:49 +0200

    Fixed api to contain comment id. All the comment operations fixed.

--- controllers/API/forum-item.js
+++ controllers/API/forum-item.js
@@ -48,7 +48,7 @@ router.post('/forum/comment/item', function (req, res) {
         date: new Date(),
         text: req.body.text,
         imgurl : req.body.imgurl
-    }
+    };
     data.db.forum.update({
             '_id': data.mongojs.ObjectId(req.body.threadID)
         }, {
@@ -87,18 +87,31 @@ router.put('/forum/thread/item', function (req, res) {
 router.put('/forum/comment/item', function (req, res) {
     // updates the modifies the comment.
     //comment is selected by its position in the array.
-    var update                                                    = {
-        "$set": {}
-    };
-    update["$set"]["comments." + req.param('arrayPos') + ".text"] = req.param("text")
-    data.db.forum.update({
-        '_id': data.mongojs.ObjectId(req.param('threadID'))
-    }, update, function (err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(200);
+    data.db.forum.findOne({
+        _id: data.mongojs.ObjectID(req.param('threadID'))
+    }, function(err, doc) {
+        var array = doc.comments;
+        var i = 0;
+        var pos;
+        for (;i< array.length;i++){
+            if (array[i].commentID === req.param('commentID')){
+                pos = i;
+            }
         }
+        console.log(pos);
+        var update= {
+            "$set": {}
+        };
+        update["$set"]["comments." + pos + ".text"] = req.param("text");
+        data.db.forum.update({
+            '_id': data.mongojs.ObjectId(req.param('threadID'))
+        }, update, function (err, user) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(200);
+            }
+        });
     });
 });
 
@@ -115,31 +128,18 @@ router.delete('/forum/thread/item', function (req, res) {
 });
 
 router.delete('/forum/comment/item', function (req, res) {
-    // deletes the thread using array index.
-    var update                                            = {
-        "$unset": {}
-    };
-    update["$unset"]["comments." + req.param('arrayPos')] = 1;
+
     data.db.forum.update({
         '_id': data.mongojs.ObjectId(req.param('threadID'))
-    }, update, function (err, user) {
+    }, {
+        $pull: {
+            comments: {commentID:req.param('commentID')}
+        }
+    }, function (err, user) {
         if (err) {
             console.log(err);
         } else {
-            //unset leaves a null behind it, the code under cleans the nulls.
-            data.db.forum.update({
-                '_id': data.mongojs.ObjectId(req.param('threadID'))
-            }, {
-                $pull: {
-                    comments: null
-                }
-            }, function (err2, user2) {
-                if (err2) {
-                    console.log(err2);
-                } else {
-                    res.send(200);
-                }
-            });
+            res.send(200);
         }
     });
 });
