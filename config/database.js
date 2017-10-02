15a0d743dd68ed0b17f888930ca3273e327fce0a
Daan <daanraatjes@gmail.com>
2017-10-02 16:47:48 +0200

    Add JSDoc and added the ability to check for multiple rights

--- config/database.js
+++ config/database.js
@@ -1,5 +1,12 @@
 var UserRights = require("./userRights.js");
 
+/**
+ * A function that validates whether the user is logged in, and if not redirect them to the log in page.
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
 exports.isLoggedIn = function(req, res, next) {
 		if (process.env.NODE_ENV === "test"){
 			return next();
@@ -8,13 +15,37 @@ exports.isLoggedIn = function(req, res, next) {
 	        return next();
 
 	    res.redirect('/');
-
 };
 
+/**
+ * A helper function to check if all rights in an array are satisfied.
+ *
+ * @param {string[]} names - A list of access rights strings
+ * @param {Object} user - The req.user object.
+ * @returns {boolean}
+ */
+function hasAllRights(names, user) {
+	var hasRights = true;
+	names.forEach(function(name) {
+		hasRight &= UserRights.userHasRights(user, name);
+	});
+	return hasRights;
+}
+
+/**
+ * A custom function that checks whether the user is authorised to perform a request (/access a webpage).
+ * It is build on top of the Passport library @see {@link http://passportjs.org/}
+ *
+ * @see {@link ./userRights.js}
+ * @param {(string|string[])} name - one or more access right strings as defined in UserRights
+ * @returns {Function}
+ */
 exports.isAuthorised = function(name) {
 	return function (req, res, next) {
 		if(req.isAuthenticated()) {
-			if(UserRights.userHasRights(req.user, name)) {
+			if(name.constructor === Array && hasAllRights(name, req.user)) {
+				return next();
+			} else if(UserRights.userHasRights(req.user, name)) {
 				return next();
 			}
 			res.send(403);
--- config/userRights.js
+++ config/userRights.js
@@ -1,8 +1,22 @@
+/**
+ * An object that should contain every user rank as a property.
+ * Every user rank should be of type [string], where every string denotes the
+ * actions that that user may perform.
+ *
+ * @type {{admin: [string], coordinator: [string]}}
+ */
 var UserRights = {
 	admin: ["ACCESS_MAIN_OVERVIEW"],
 	coordinator: []
-}
+};
 
+/**
+ * Validates if a user with a certain rank is allowed to perform a certain action.
+ *
+ * @param {Object} user - The req.user object
+ * @param {string} name
+ * @returns {boolean}
+ */
 exports.userHasRights = function(user, name) {
 	return user.rank ? UserRights[user.rank].indexOf(name) > -1 : false;
 };
