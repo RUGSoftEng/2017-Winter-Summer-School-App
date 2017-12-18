46ffa73eeae4eaaed9c00da2ef4ecaef034f449d
Nikolas Hadjipanayi <nikolas.hadjipanayi@gmail.com>
2017-12-18 19:28:19 +0200

    changes for allowing first login

--- config/passport.js
+++ config/passport.js
@@ -3,37 +3,46 @@ const bcrypt        = require('bcrypt-nodejs');
 var Users = require('mongoose').model('account');
 
 module.exports = function (passport) {
-
-	passport.use('login', new LocalStrategy(
-		function (usern, password, done) {
-			Users.findOne({username: usern}, function (err, user) {
-				if (typeof user !== 'undefined' && !err) {
-					bcrypt.compare(password, user.password, function (err, res) {
-						if (res == true) {
-							return done(null, user);
-						} else {
-							return done(null, false, {"message": "Invalid password."});
-						}
-					});
-
-				} else {
-					return done(null, false, {"message": "Invalid username."});
+    Users.count({},function(err, count) {
+		passport.use('login', new LocalStrategy(
+			function (usern, password, done) {
+				if(count === 0)
+				{
+					return done(null,{_id:"firstlogin"})
 				}
-			});
-		})
-	);
+				Users.findOne({username: usern}, function (err, user) {
+					if (typeof user !== 'undefined' && !err) {
+						bcrypt.compare(password, user.password, function (err, res) {
+							if (res == true) {
+								return done(null, user);
+							} else {
+								return done(null, false, {"message": "Invalid password."});
+							}
+						});
 
-	passport.serializeUser(function (user, done) {
+					} else {
+						return done(null, false, {"message": "Invalid username."});
+					}
+				});
+			})
+	);
+    });
+
+    passport.serializeUser(function (user, done) {
 		done(null, user._id);
 	});
 
 	passport.deserializeUser(function (id, done) {
-		Users.findOne({_id: id}, function (err, user) {
-
-			if (typeof user === 'undefined' || err)
-				console.log('Error in deserializing user');
-			done(null, user);
-		});
+        if(id === "firstlogin") {
+            done(null, user);
+        }
+        else {
+            Users.findOne({_id: id}, function (err, user) {
+                if (typeof user === 'undefined' || err)
+                    console.log('Error in deserializing user');
+                done(null, user);
+            });
+		}
 	});
 
 };
