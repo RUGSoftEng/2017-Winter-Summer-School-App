790725c709c91f8e132796451720488d4245f7bf
Nikolas Hadjipanayi <nikolas.hadjipanayi@gmail.com>
2018-01-14 13:22:45 +0100

    minor changes as discussed in PR

--- models/forum.js
+++ models/forum.js
@@ -1,77 +1,78 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+	Schema = mongoose.Schema;
 
 var ForumSchema = new Schema({
-    title: {
-        type: String,
-        required: 'Title can not be empty',
-        trim: true
-    },
-    description: {
-        type: String,
-        required: 'Description can not be empty',
-        trim: true
-    },
-    created: {
-        type: Date,
-        default: Date.now()
-    },
-    author: {
-        type: String,
-        required: 'A thread needs an author',
-        trim: true
-    },
-    posterID: {
-        type: String,
-        trim: true,
-        required: 'A thread needs the id of the poster'
-    },
-    imgURL: {
-        type: String,
-        trim: true
-    },
-    comments: {
-        type: [String],
-        default: []
-    },
-    edited: {
-        type: Date
-    }
+	title: {
+		type: String,
+		required: 'Title can not be empty',
+		trim: true
+	},
+	description: {
+		type: String,
+		required: 'Description can not be empty',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now()
+	},
+	author: {
+		type: String,
+		required: 'A thread needs an author',
+		trim: true
+	},
+	posterID: {
+		type: String,
+		trim: true,
+		required: 'A thread needs the id of the poster'
+	},
+	imgURL: {
+		type: String,
+		trim: true
+	},
+	comments: {
+		type: [String],
+		default: []
+	},
+	edited: {
+		type: Date
+	}
 });
 mongoose.model('forum', ForumSchema);
 
 var CommentSchema = new Schema({
-    text: {
-        type: String,
-        required: 'A comment needs to have a text',
-        trim: true
-    },
-    created: {
-        type: Date,
-        default: Date.now()
-    },
-    author: {
-        type: String,
-        required: 'A comment needs an author',
-        trim: true
-    },
-    posterID: {
-        type: String,
-        trim: true,
-        required: 'A comment needs the id of the poster'
-    },
-    imgURL: {
-        type: String,
-        trim: true
-    },
-    parentThread: {
-        type: String,
-        required: 'A comment needs a parent thread'
-    },
-    edited: {
-        type: Date
-    }
+	text: {
+		type: String,
+		required: 'A comment needs to have a text',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now()
+	},
+	author: {
+		type: String,
+		required: 'A comment needs an author',
+		trim: true
+	},
+	posterID: {
+		type: String,
+		trim: true,
+		required: 'A comment needs the id of the poster'
+	},
+	imgURL: {
+		type: String,
+		default: "",
+		trim: true
+	},
+	parentThread: {
+		type: String,
+		required: 'A comment needs a parent thread'
+	},
+	edited: {
+		type: Date
+	}
 });
 mongoose.model('comment', CommentSchema);
