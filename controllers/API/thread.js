ec6ff541229c90f234873c03ea2b09d91c8f0636
Nikolas Hadjipanayi <nikolas.hadjipanayi@gmail.com>
2018-01-15 19:44:54 +0100

    small consistency fixes

--- controllers/API/thread.js
+++ controllers/API/thread.js
@@ -2,97 +2,98 @@ const express = require('express');
 const router = express.Router();
 const Forum = require('mongoose').model('forum');
 const Comment = require('mongoose').model('comment');
+const logger = require(process.cwd() + '/config/lib/logger');
 
 
 router.post('/API/forum/thread', function (req, res) {
-    var newThread = new Forum(req.body);
-    newThread.save(function (err, result) {
-        if (err) {
-            res.status(201);
-            res.send(err);
-            console.log(err);
-        } else {
-            console.log(result);
-            res.sendStatus(200);
-        }
-    });
+	var newThread = new Forum(req.body);
+	newThread.save(function (err, result) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			logger.warning("Can't add thread into the database " + err);
+		} else {
+			logger.warning(result);
+			res.sendStatus(200);
+		}
+	});
 });
 
 router.delete('/API/forum/thread', function (req, res) {
-    Forum.findOne({
-            '_id': req.query.id
-        }, function (err, thread) {
-            if (err) {
-                res.status(201);
-                res.send(err);
-                console.log("error finding the thread with the id specified", err)
-            }
-            else {
-                Comment.deleteMany({
-                    '_id': {$in: thread.comments}
-                }, function (err2) {
-                    if (err2) {
-                        res.status(201);
-                        res.send(err2);
-                        console.log("error deleting the comments associated with the thread", err2);
-                    }
-                    else {
-                        Forum.findOneAndRemove({
-                            '_id': req.query.id
-                        }, function (err3) {
-                            if (err) {
-                                res.status(201);
-                                res.send(err);
-                                console.log("error deleting the thread", err)
-                            }
-                            else {
-                                res.sendStatus(200);
-                            }
-                        })
+	Forum.findOne({
+			'_id': req.query.id
+		}, function (err, thread) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				logger.warning("can't find the thread with the id specified ", err)
+			}
+			else {
+				Comment.deleteMany({
+					'_id': { $in: thread.comments }
+				}, function (err2) {
+					if (err2) {
+						res.status(400);
+						res.send(err2);
+						logger.warning("can't delete the comments associated with the thread ", err2);
+					}
+					else {
+						Forum.findOneAndRemove({
+							'_id': req.query.id
+						}, function (err3) {
+							if (err3) {
+								res.status(400);
+								res.send(err3);
+								logger.warning("can't delete the thread from the database ", err)
+							}
+							else {
+								res.sendStatus(200);
+							}
+						})
 
-                    }
-                });
-            }
-        }
-    );
+					}
+				});
+			}
+		}
+	);
 });
 
 router.put('/API/forum/thread', function (req, res) {
-    Forum.findOneAndUpdate({
-        '_id': req.query.id
-    }, {
-        $set: {
-            title: req.query.title,
-            description: req.query.description,
-            edited: Date.now()
-        }
-    }, function (err) {
-        if (err) {
-            res.status(201);
-            res.send(err);
-            console.log(err);
-        }
-        else {
-            res.sendStatus(200);
-        }
-    });
+	Forum.findOneAndUpdate({
+		'_id': req.query.id
+	}, {
+		$set: {
+			title: req.query.title,
+			description: req.query.description,
+			edited: Date.now()
+		}
+	}, function (err) {
+		if (err) {
+			res.status(400);
+			res.send(err);
+			logger.warning("can't edit the thread " + err);
+		}
+		else {
+			res.sendStatus(200);
+		}
+	});
 });
 
 router.get('/API/forum/thread', function (req, res) {
-    Forum
-        .find({})
-        .sort({$natural: -1})
-        .limit(req.query.count || 200)
-        .exec(function (err, threads) {
-            if (err) {
-                res.status(201);
-                res.send(err);
-                console.log(err);
-            }
-            else {
-                res.status(200);
-                res.send(threads);
-            }
-        });
+	Forum
+		.find({})
+		.sort({ $natural: -1 })
+		.limit(req.query.count || 200)
+		.exec(function (err, threads) {
+			if (err) {
+				res.status(400);
+				res.send(err);
+				logger.warning("can't get the threads from the database " + err);
+			}
+			else {
+				res.status(200);
+				res.send(threads);
+			}
+		});
 });
 module.exports = router;
