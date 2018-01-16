99393b6057d8fb964cfbac27aae19462bde9bf00
Daan Raatjes <daanraatjes@gmail.com>
2018-01-17 00:41:53 +0100

    Set max overview items and remove overflow

--- public/dist/js/controllers/AnnouncementController.js
+++ public/dist/js/controllers/AnnouncementController.js
@@ -1,6 +1,6 @@
 app.controller('AnnouncementController', ['$scope', '$http', function($scope, $http) {
 
-	$http.get('/API/announcement')
+	$http.get('/API/announcement?count=3')
 		.then(function(data) {
 			$scope.announcements = data.data;
 		}, function(err) {
--- public/dist/js/controllers/GeneralinformationController.js
+++ public/dist/js/controllers/GeneralinformationController.js
@@ -1,5 +1,5 @@
 app.controller('GeneralinfoController', ['$scope', '$http', function($scope, $http) {
-	$http.get('/API/generalinfo')
+	$http.get('/API/generalinfo?count=3')
 		.then(function(data) {
 			$scope.generalinfo = data.data;
 		}, function(err) {
