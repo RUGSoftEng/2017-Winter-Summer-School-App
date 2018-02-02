161c147a4652f27103d472963e8c9d42450743d5
Daan Raatjes <daanraatjes@gmail.com>
2018-02-02 20:02:39 +0100

    Make announcements school specific

--- public/dist/js/controllers/AnnouncementController.js
+++ public/dist/js/controllers/AnnouncementController.js
@@ -1,6 +1,6 @@
 app.controller('AnnouncementController', ['$scope', '$http', function($scope, $http) {
 
-	$http.get('/API/announcement?count=3')
+	$http.get('/API/announcement?count=3&school=' + $scope.school)
 		.then(function(data) {
 			$scope.announcements = data.data;
 		}, function(err) {
